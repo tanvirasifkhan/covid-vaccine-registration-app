483e469982604f5adccf1eea594bd007473f046f
Asif Khan <asifkhan.github@gmail.com>
2024-10-09 08:17:59 +0600

    vaccine candidates migration created

---
+++ backend/database/migrations/2024_10_09_020234_create_vaccine_candidates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vaccine_candidates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('center_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->dateTime('scheduled_at');
+            $table->enum('status', ['scheduled', 'vaccinated'])->nullable();
+            $table->timestamps();
+            $table->foreign('center_id')->references('id')->on('vaccine_centers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vaccine_candidates');
+    }
+};
