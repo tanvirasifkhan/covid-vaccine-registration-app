77bb13ce75611a58364701f1d9dead663791f3ae
Asif Khan <asifkhan.github@gmail.com>
2024-10-09 21:33:32 +0600

    read methods declared

--- backend/app/Interfaces/VaccineCandidateRepositoryInterface.php
+++ backend/app/Interfaces/VaccineCandidateRepositoryInterface.php
@@ -3,8 +3,14 @@
 namespace App\Interfaces;
 
 use App\Models\VaccineCandidate;
+use Illuminate\Database\Eloquent\Collection;
 
 interface VaccineCandidateRepositoryInterface
 {
     public function store(array $data): VaccineCandidate;
+
+    public function all(): Collection;
+
+    public function fetch(int $id): VaccineCandidate | NULL;
+
 }
