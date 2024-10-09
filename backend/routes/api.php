49aa3c050ceb4cb9f68a1dfc862d99e5b50ac153
Asif Khan <asifkhan.github@gmail.com>
2024-10-09 11:07:15 +0600

    login logout endpoints added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthLoginController;
+use App\Http\Controllers\Auth\AuthLogoutController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+/******************* Authentication API Endpoints *******************************/
+Route::post('login', AuthLoginController::class);
+Route::post('logout', AuthLogoutController::class)->middleware('auth:sanctum');
