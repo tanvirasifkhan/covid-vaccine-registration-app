ce308712b2a995827e9eb86d8a3b49acb1ef9d44
Asif Khan <asifkhan.github@gmail.com>
2024-10-09 21:33:44 +0600

    read methods implemented

--- backend/app/Repositories/VaccineCandidateRepository.php
+++ backend/app/Repositories/VaccineCandidateRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Interfaces\VaccineCandidateRepositoryInterface;
 use App\Models\VaccineCandidate;
+use Illuminate\Database\Eloquent\Collection;
 
 class VaccineCandidateRepository implements VaccineCandidateRepositoryInterface
 {
@@ -19,5 +20,21 @@ class VaccineCandidateRepository implements VaccineCandidateRepositoryInterface
             'phone' => $data['phone'],
             'nid' => $data['nid']
         ]);
+    }
+
+    /**
+     * fetch all the vaccine candidates
+     */
+    public function all(): Collection
+    {
+        return VaccineCandidate::with('vaccineCenter')->orderBy('id', 'DESC')->get();
+    }
+
+    /**
+     * fetch candidate details by id
+     */
+    public function fetch(int $id): VaccineCandidate | NULL
+    {
+        return VaccineCandidate::with('vaccineCenter')->where('id', $id)->first();
     }
 }
