e1b74e4d50b415a9b08553cebc94a5587fa688c0
Asif Khan <asifkhan.github@gmail.com>
2024-10-10 22:05:41 +0600

    enum to array convertion method added

--- backend/app/Enums/VaccineCandidateStatus.php
+++ backend/app/Enums/VaccineCandidateStatus.php
@@ -4,6 +4,15 @@ namespace App\Enums;
 
 enum VaccineCandidateStatus: string
 {
+    public static function toArray(): array {
+        return array_map(
+            fn(self $enum) => $enum->value, 
+            self::cases()
+        );
+    }
+    
+    case ALL = "all";
+    case REGISTERED = "registered";
     case SCHEDULED = "scheduled";
     case VACCINATED = "vaccinated";
 }
