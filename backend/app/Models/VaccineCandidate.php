da9d46ffde8d80249c8ccd53dd3d56afa1e07531
Asif Khan <asifkhan.github@gmail.com>
2024-10-11 01:03:28 +0600

    notifiable trait added

--- backend/app/Models/VaccineCandidate.php
+++ backend/app/Models/VaccineCandidate.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\VaccineCenter;
+use Illuminate\Notifications\Notifiable;
 
 class VaccineCandidate extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $table = "vaccine_candidates";
 
