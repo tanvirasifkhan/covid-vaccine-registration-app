ae93ffd2cbd4db697f74463d814a0b9a095a9e35
Asif Khan <asifkhan.github@gmail.com>
2024-10-11 07:18:09 +0600

    schedule commands modified

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -5,6 +5,6 @@ use App\Models\VaccineCandidate;
 use App\Notifications\VaccineCandidateScheduleEmailNotification;
 use App\Console\Commands\SendScheduleEmail;
 
-Schedule::command('send:vaccine-schedule-email')->timezone('Asia/Dhaka')->at("06:17");
+Schedule::command('send:vaccine-schedule-email')->timezone('Asia/Dhaka')->withoutOverlapping()->at("21:00");
 
-Schedule::command('change:candidate-status-to-vaccinated')->timezone('Asia/Dhaka')->at("06:27");
+Schedule::command('change:candidate-status-to-vaccinated')->timezone('Asia/Dhaka')->at("24:01");
